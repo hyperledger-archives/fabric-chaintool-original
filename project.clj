3001adfa91b4143466b7454cec05168249f47f0a
Gregory Haskins <gregory.haskins@gmail.com>
2016-01-21 14:25:14 -0500

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.cli "0.3.3"]
+                 [instaparse "1.4.1"]]
   :main ^:skip-aot cljparse.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/cljparse/core.clj
+++ src/cljparse/core.clj
@@ -1,7 +1,28 @@
 (ns cljparse.core
+  (:require [instaparse.core :as insta]
+            [clojure.java.io :as io]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def cli-options
+  ;; An option with a required argument
+  [["-p" "--path PATH" "path to project to build"
+    :default "./"
+    :validate [#(.isDirectory (io/file %))]]
+   ["-h" "--help"]])
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn -main [& args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    ;; Handle help and error conditions
+    (cond (or (:help options) (not= errors nil))
+          (exit 0 summary))
+
+    (println "Starting cljparse with path:" (:path options) "and errors:" errors)))
+
+
+
+
