7a4aacbd28eeaf67d32d57c1931bb67c319b0318
Gregory Haskins <gregory.haskins@gmail.com>
2016-01-22 13:36:10 -0500

    Add a more formal skip-parser
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- src/cljparse/config/parser.clj
+++ src/cljparse/config/parser.clj
@@ -3,9 +3,9 @@
             [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(def whitespace (insta/parser "whitespace = #'[ \t]*'"))
+(def skipper (insta/parser (io/resource "skip.bnf")))
 
-(def grammar (insta/parser (io/resource "config.bnf") :auto-whitespace whitespace))
+(def grammar (insta/parser (io/resource "config.bnf") :auto-whitespace skipper))
 
 (defn parser [file]
   (grammar (slurp file)))
