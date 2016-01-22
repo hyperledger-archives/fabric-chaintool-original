6666c8a08df33ebf21da6f2646de6bcb67451530
Eric Baur <ecb@robeybaur.org>
2016-01-22 09:49:41 -0500

    slurp file to get a string (strip the newline) make (-main) repl-friendly
    Signed-off-by: Eric Baur <ecb@robeybaur.org>

--- src/cljparse/config/parser.clj
+++ src/cljparse/config/parser.clj
@@ -1,8 +1,9 @@
 (ns cljparse.config.parser
   (:require [instaparse.core :as insta]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (def grammar (insta/parser (io/resource "config.bnf")))
 
 (defn parser [file]
-  (grammar file))
+  (grammar (str/trim-newline(slurp file))))
--- src/cljparse/core.clj
+++ src/cljparse/core.clj
@@ -9,33 +9,32 @@
 
 (def cli-options
   ;; An option with a required argument
-  [["-p" "--path PATH" "path to project to build"
-    :default "./"]
+  [["-p" "--path PATH" "path to project to build" :default "./"]
    ["-h" "--help"]])
 
 (defn exit [status msg]
-  (println msg)
-  (System/exit status))
+  (throw (Exception. msg)))
 
 (defn -main [& args]
-  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
-    ;; Handle help and error conditions
-    (cond (:help options)
-          (exit 0 summary))
+  (let [ {:keys [options arguments errors summary]} (parse-opts args cli-options) ]
+         (cond (:help options)
+              (do
+                (println summary)
+                0)
 
-    (cond (not= errors nil)
-          (exit -1 (str "Error: " errors)))
+              (not= errors nil)
+              (do
+                (println "Error: " errors)
+                -1)
 
-    (let [file (io/file (:path options) configname)]
-      (cond (not (.isFile file))
-            (exit -1 (str "Configuration not found at " (:path options))))
-
-      (println "Starting cljparse with path:" (:path options) "and errors:" errors)
-      (config/parser (io/file (:path options) configname)))))
-
-
-
-
-
-
-
+              :else (let [ path (:path options)
+                           file (io/file path configname) ]
+                      (do
+                        (cond (not (.isFile file))
+                            (do
+                              (println "Configuration not found at " path)
+                              -1)
+                            :else
+                            (do
+                              (println "Starting cljparse with path:" path "and errors:" errors)
+                              (config/parser file))))))))
