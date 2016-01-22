9c0fa6202a0acfa9c7857e05e5167d5a7180abe9
Gregory Haskins <gregory.haskins@gmail.com>
2016-01-22 10:40:24 -0500

    Update to clojure v1.8
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.3"]
                  [instaparse "1.4.1"]]
   :main ^:skip-aot cljparse.core
