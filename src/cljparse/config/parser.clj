3df48e144a1ee51290a1a30bff04ad67065d403d
Gregory Haskins <gregory.haskins@gmail.com>
2016-01-21 21:23:02 -0500

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- src/cljparse/config/parser.clj
+++ src/cljparse/config/parser.clj
@@ -2,5 +2,7 @@
   (:require [instaparse.core :as insta]
             [clojure.java.io :as io]))
 
+(def grammar (insta/parser (io/resource "config.bnf")))
+
 (defn parser [file]
   (println "Parsing" file))
