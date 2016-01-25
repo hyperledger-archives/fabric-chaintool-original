0d831feb8f09610785b80374110ca809c07080f0
Gregory Haskins <gregory.haskins@gmail.com>
2016-01-25 17:15:18 -0500

    Add config.parser/find
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- src/cljparse/config/parser.clj
+++ src/cljparse/config/parser.clj
@@ -1,11 +1,41 @@
 (ns cljparse.config.parser
   (:require [instaparse.core :as insta]
             [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.zip :as zip]))
 
 (def skipper (insta/parser (io/resource "skip.bnf")))
 
 (def grammar (insta/parser (io/resource "config.bnf") :auto-whitespace skipper))
 
-(defn parser [file]
-  (grammar (slurp file)))
+(defn parser [file] (->> file slurp grammar zip/vector-zip))
+
+(defn isnode? [node] (if (and (vector? node) (keyword? (first node))) :true nil))
+(defn nodename [node] (if (isnode? node) (first node) nil))
+
+(defn fqp [_loc]
+  (loop [loc _loc
+         path ()]
+    (cond
+
+         (= loc nil)
+         (into [] path)
+
+         :else
+         (recur (zip/up loc) (conj path (nodename (zip/node loc)))))))
+
+(defn find [path tree]
+  (loop [loc tree]
+    (cond
+
+      (zip/end? loc)
+      nil
+
+      (= (fqp loc) path)
+      (rest (zip/node loc))
+
+      :else
+      (recur (zip/next loc)))))      
+     
+
+
