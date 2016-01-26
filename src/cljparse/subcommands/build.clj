22b719fac5b3bf775603b8d0d873344dcf1d9e2d
Gregory Haskins <gregory.haskins@gmail.com>
2016-01-26 12:30:13 -0500

    Add interface coalecing
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- src/cljparse/subcommands/build.clj
+++ src/cljparse/subcommands/build.clj
@@ -1,5 +1,11 @@
-(ns cljparse.subcommands.build)
+(ns cljparse.subcommands.build
+  (:require [cljparse.config.parser :as config]))
+
+(defn interfaces [config]
+  (let [keys [[:configuration :provides] [:configuration :consumes]]]
+        (->> (map #(config/find % config) keys) flatten (into #{}))))
 
 (defn run [path config]
-  (println "Build using configuration: " config))
+  (println "Build using configuration: " config)
+  (println "Interfaces: " (interfaces config)))
 
