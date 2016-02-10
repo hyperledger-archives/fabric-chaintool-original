04bf276a82b598b6b437b36ea99bd962b520d3df
Gregory Haskins <gregory.haskins@gmail.com>
2016-02-10 10:33:34 -0500

    Use lein-bin to generate executable
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject obcc "_VERSION_"
+(defproject obcc "0.1_SNAPSHOT"
   :description "openblockchain compiler"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-bin "0.3.5"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.3"]
                  [org.clojure/algo.generic "0.1.2"]
@@ -13,6 +14,9 @@
                  [me.raynes/fs "1.4.6"]
                  [clojurewerkz/propertied "1.2.0"]]
   :main ^:skip-aot obcc.core
+  :bin {:name "obcc"
+        :bin-path "target"
+        :bootclasspath true}
   :target-path "target/%s"
   :resource-paths ["resources" "_PROPSDIR_"]
   :profiles {:uberjar {:aot :all}})
