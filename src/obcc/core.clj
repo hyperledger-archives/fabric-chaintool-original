89fe9c1ade100e45d89219674851ffe42ea077b7
Eric Baur <ecb@robeybaur.org>
2016-02-09 19:18:20 -0500

    Factor out the core app to run in a repl
    Signed-off-by: Eric Baur <ecb@robeybaur.org>

--- src/obcc/core.clj
+++ src/obcc/core.clj
@@ -19,7 +19,7 @@
 (defn exit [status msg & rest]
   (do
     (apply println msg rest)
-    (System/exit status)))
+    status))
 
 (def subcommands
   {"build",   ["Build the chaincode project",          buildcmd/run],
@@ -42,37 +42,38 @@
                  "Please refer to the manual page for more information."])
        (string/join \newline)))
 
-(defn -main [& args]
+(defn -app [& args]
   (let [ {:keys [options arguments errors summary]} (parse-opts args cli-options) ]
-    (cond
+    (cond (not= errors nil)
+          (exit -1 "Error: " (string/join errors))
 
-      (:help options)
-      (exit 0 (usage summary))
+          (:help options)
+          (exit 0 (usage summary))
 
-      (:version options)
-      (exit 0 (version))
+          (:version options)
+          (exit 0 (version))
 
-      (= (count arguments) 0)
-      (exit 0 (usage summary))
+          (= (count arguments) 0)
+          (exit 0 (usage summary))
 
-      (not= errors nil)
-      (exit -1 "Error: " (string/join errors))
+          (not= (count arguments) 1)
+          (exit -1 "Error: bad argument count")
 
-      (not= (count arguments) 1)
-      (exit -1 "Error: bad argument count")
+          :else
+          (let [path (:path options)
+                file (io/file path util/configname)]
+            (do
+              (cond (not (.isFile file))
+                    (exit -1 "Configuration not found at " path)
+                    :else
+                    (let [config (config/parser file)]
+                      (if-let [[_ func] (subcommands (first arguments))]
+                        (do
+                          (try
+                            (func path config)
+                            (catch Exception e (println "error:" (:stderr (ex-data e))))))
+                          (exit 0 "done"))
+                        (exit 1 (usage summary)))))))))
 
-      :else
-      (let [path (:path options)
-            file (io/file path util/configname)]
-        (do
-          (cond (not (.isFile file))
-                (exit -1 "Configuration not found at " path)
-                :else
-                (let [config (config/parser file)]
-                  (if-let [[_ func] (subcommands (first arguments))]
-                    (do
-                      (try
-                        (func path config)
-                        (catch Exception e (println "error:" (:stderr (ex-data e)))))
-                      (System/exit 0))
-                    (exit 1 (usage summary))))))))))
+(defn -main [& args]
+  (System/exit (apply -app args)))
