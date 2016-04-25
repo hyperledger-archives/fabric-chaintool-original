437103357f103e7cc7c81fadda6307031624fa25
Gregory Haskins <gregory.haskins@gmail.com>
2016-04-25 13:33:13 -0400

    lein-release plugin: preparing 0.5.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chaintool "0.5.1-SNAPSHOT"
+(defproject chaintool "0.5.1"
   :description "hyperledger chaincode tool"
   :url "https://github.com/ghaskins/chaintool"
   :license {:name "Apache License"
