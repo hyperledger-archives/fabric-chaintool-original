4fdf049247b94e48b3bc325ef32ab8dabee6bbb1
Gregory Haskins <gregory.haskins@gmail.com>
2016-05-02 13:45:43 -0400

    lein-release plugin: bumped version from 0.6 to 0.7-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chaintool "0.6"
+(defproject chaintool "0.7-SNAPSHOT"
   :description "hyperledger chaincode tool"
   :url "https://github.com/ghaskins/chaintool"
   :license {:name "Apache License"
