cd29f56b8d670ec53505214e619a77f334edf30c
Gregory Haskins <gregory.haskins@gmail.com>
2016-08-24 22:49:39 -0400

    Prepare for v0.9.1 development
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chaintool "0.9.0"
+(defproject chaintool "0.9.1-SNAPSHOT"
   :description "hyperledger chaincode tool"
   :url "https://github.com/ghaskins/chaintool"
   :license {:name "Apache License"
